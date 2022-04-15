610e3178bce82f9803bc55b58e753842b42cf834
rabiul1399 <www.rabiulislam3808@gmail.com>
2022-04-15 22:42:43 +0600

    second commite

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -6,7 +6,7 @@ const Expert = ({ expert }) => {
         <div className=' g-5 col-sm-12 col-md-6 col-lg-4 '>
 
  
-        <div className="card " style={{width: "18rem"}} >
+        <div className="card "  >
             
             <img src={img} className="card-img-top" alt="..."/>
                 <div className="card-body">
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -15,7 +15,7 @@ const Services = () => {
 
     return (
         <div>
-            <h2  className='services-title mt-5'>Our Services</h2>
+            <h2  className='text-center text-primary mt-5'>Our Services</h2>
            
             <div className='car-services'>
                 {
